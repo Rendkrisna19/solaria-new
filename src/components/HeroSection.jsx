e772193c90a724965f3240b3a4c1335f24dba658
Nama Anda <muhammadrendykrisna@mail.com>
2025-04-08 10:11:41 +0700

    update lagu

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,7 +4,7 @@ import Img1 from "../assets/solaria-header.jpg"
 
 const HeroSection = () => {
   return (
-    <div className="relative w-full h-screen flex items-center justify-center px-4 md:px-10 lg:px-20 pt-40 md:pt-42 ">
+    <div className="relative w-full h-screen flex items-center justify-center px-4 md:px-10 lg:px-20  ">
       {/* Hero Image */}
       <div className="w-full max-w-6xl mx-auto h-[80vh] md:h-[85vh] rounded-lg overflow-hidden shadow-lg relative mb-20">
         {/* Gambar */}
--- src/pages/Menu-page/index.jsx
+++ src/pages/Menu-page/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ListMenu from "./List_menu";
 import ListDropdown from "./ListDropdown";
 import bannerImage from "../../assets/bannnersolaria.png";
-import Bgpaper from "../../assets/paperpink.jpg";
+// import Bgpaper from "../../assets/paperpink.jpg";
 import Scroll from "./ScrollTop"
 import DownlodMenu from "./DwnldMenu"
 
@@ -10,7 +10,7 @@ export default function Menu() {
   return (
     <div
       className="pt-20 md:pt-24 bg-cover bg-center min-h-screen"
-      style={{ backgroundImage: `url(${Bgpaper})` }}
+      // style={{ backgroundImage: `url(${Bgpaper})` }}
     >
       {/* Optional: bisa taruh juga overlay warna jika ingin kontras teks lebih jelas */}
       <div className="w-full min-h-screen">
@@ -26,8 +26,8 @@ export default function Menu() {
 
         {/* Title section */}
         <div className="pt-8  text-center">
-          <h2 className="text-4xl font-semibold text-white">
-           Menu Kami
+          <h2 className="text-4xl font-semibold text-black">
+           Menu <span className="text-pink-500">Kami</span>
           </h2>
         </div>
 
