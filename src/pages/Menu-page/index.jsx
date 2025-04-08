4ee2d241a18b162317aa667e7c2b51061ec78ef5
Nama Anda <muhammadrendykrisna@mail.com>
2025-04-08 10:39:23 +0700

    update lagi lagi

--- src/pages/Menu-page/index.jsx
+++ src/pages/Menu-page/index.jsx
@@ -10,24 +10,26 @@ export default function Menu() {
   return (
     <div
       className="pt-20 md:pt-24 bg-cover bg-center min-h-screen"
-      // style={{ backgroundImage: `url(${Bgpaper})` }}
+    // style={{ backgroundImage: `url(${Bgpaper})` }}
     >
       {/* Optional: bisa taruh juga overlay warna jika ingin kontras teks lebih jelas */}
       <div className="w-full min-h-screen">
 
-        {/* Full banner image */}
-        <div className="w-full">
+        <div className="w-full relative">
           <img
             src={bannerImage}
             alt="Banner Menu"
-            className="w-full h-auto object-cover"
+            className="w-full h-auto object-cover transition-opacity duration-300 ease-in-out"
+            loading="eager"
           />
+          <div className="absolute inset-0 bg-gray-200 animate-pulse z-[-1]"></div>
         </div>
+
 
         {/* Title section */}
         <div className="pt-8  text-center">
           <h2 className="text-4xl font-semibold text-black">
-           Menu <span className="text-pink-500">Kami</span>
+            Menu <span className="text-pink-500">Kami</span>
           </h2>
         </div>
 
@@ -36,8 +38,8 @@ export default function Menu() {
 
         {/* Komponen dropdown */}
         <ListDropdown />
-        <DownlodMenu/>
-        <Scroll/>
+        <DownlodMenu />
+        <Scroll />
       </div>
     </div>
   );
